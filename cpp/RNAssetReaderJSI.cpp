fa05605e8bea453425de5052a8fd4882d1821689
Stephen <stepan.kopylov@ibitcy.com>
2022-12-14 03:46:33 +0200

    test

--- cpp/RNAssetReaderJSI.cpp
+++ cpp/RNAssetReaderJSI.cpp
@@ -1,5 +1,8 @@
 #include "RNAssetReaderJSI.hpp"
 #include <string>
+#include <CoreFoundation/CoreFoundation.h>
+#include <fstream>
+#include <sstream>
 
 //#ifdef __ANDROID__
 //
@@ -28,8 +31,18 @@ void install(Runtime &jsiRuntime, std::shared_ptr<CallInvoker> callInvoker) {
 
 static Value read(Runtime &rt, TurboModule &turboModule,
 						 const Value *args, size_t arg_count){
+	CFURLRef manifest_url = CFBundleCopyResourceURL(CFBundleGetMainBundle(), CFSTR("main"), CFSTR("json"), NULL);
+	char manifest_path[PATH_MAX];
 	
-	return 10;
+	CFURLGetFileSystemRepresentation(manifest_url, true, (UInt8 *)manifest_path, PATH_MAX);
+	
+	CFRelease(manifest_url);
+	
+	std::ifstream myfile(manifest_path);
+	std::stringstream buffer;
+	buffer << myfile.rdbuf();
+	
+	return String::createFromUtf8(rt, buffer.str());
 };
 
 RNAssetReaderTurboModule::RNAssetReaderTurboModule(Runtime &jsiRuntime, std::shared_ptr <CallInvoker> jsInvoker) : TurboModule("RNAssetReaderTurboModule", jsInvoker){
--- example/App.tsx
+++ example/App.tsx
@@ -65,7 +65,7 @@ const App = () => {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const test = RNAssetReader.read();
+  const test = JSON.parse(RNAssetReader.read());
 
   console.log('test ', test);
 
